6cba6f70ab889c7e7d5f36572936f38473bf23b5
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-10 17:38:44 +0530

    commit add

--- L01Mathematics/printPattern.java
+++ L01Mathematics/printPattern.java
@@ -1,27 +1,65 @@
+// package DSA_GFG.L01Mathematics;
+
+// import java.util.*;
+
+// public class printPattern {
+    
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         while(n > 0){
+//             int m = sc.nextInt();
+//             printPattern p = new printPattern();
+//             p.printPat(m);
+//             System.out.println();
+//             n--;
+//         }
+//         sc.close();
+//     }
+
+//     private void printPat(int m) {
+//         for (int i = m; i > 0; i--) {
+//             for (int j = 0; j < m; j++) {
+//                 System.out.print(i + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
 package DSA_GFG.L01Mathematics;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class printPattern {
     
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        while(n>0){
+        while (n > 0) {
             int m = sc.nextInt();
             printPattern p = new printPattern();
             p.printPat(m);
             System.out.println();
             n--;
         }
-
+        sc.close();
     }
-
 
     private void printPat(int m) {
-        
+        for (int i = m; i >= 1; i--) {
+            for (int j = m; j >= 1; j--) {
+                for (int k = 0; k < i; k++) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.print("$");
+        }
     }
-    
 }
